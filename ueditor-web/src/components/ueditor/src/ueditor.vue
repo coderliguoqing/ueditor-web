02a91dd6f4c0fffc57fdd5782495322e89eedb34
liguoqing <liguoqing1@bluemoon.com.cn>
2017-11-29 16:14:14 +0800

    初始化项目

---
+++ ueditor-web/src/components/ueditor/src/ueditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <script id="editor" type="text/plain" ></script>
+  </div>
+</template>
+
+<script>
+  import '../../../../static/ueditor/ueditor.config.js'
+  import '../../../../static/ueditor/ueditor.all.js'
+  import '../../../../static/ueditor/lang/zh-cn/zh-cn.js'
+
+  export default {
+    name: "UEditor",
+    props: {
+      id: {
+          type: String
+      },
+      config: {
+          type: Object
+      }
+    },
+    data() {
+      return {
+        editor: null
+      }
+    },
+    mounted() {
+      //初始化UE
+      const _this = this;
+      this.editor = UE.delEditor("editor");
+      this.editor = UE.getEditor('editor',this.config);
+    },
+    destoryed() {
+      this.editor.destory();
+    },
+    methods:{
+      getUEContent: function(){
+       return this.editor.getContent();
+      },
+    }
+  }
+</script>
---
+++ ueditor-web/src/pages/ueditor_demo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+      <div>
+        <button size="primary" type="info" icon="plus" @click="getContent">获取内容</button>
+        <UEditor :config=config ref="ueditor"></UEditor>
+      </div>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+  import {UEditor} from '@/components/ueditor/index.js'
+
+  export default{
+      name: 'hello',
+      components: {UEditor},
+      data(){
+        return {
+          config: {
+            //可以在此处定义工具栏的内容
+            // toolbars: [
+            //  ['fullscreen', 'undo', 'redo','|','bold', 'italic', 'underline',
+            //  '|','superscript','subscript','|', 'insertorderedlist', 'insertunorderedlist',
+            //  '|','fontfamily','fontsize','justifyleft','justifyright','justifycenter','justifyjustify']
+            // ],
+            autoHeightEnabled: false,
+            autoFloatEnabled: true,
+            initialContent:'请输入内容',   //初始化编辑器的内容,也可以通过textarea/script给值，看官网例子
+            autoClearinitialContent:true, //是否自动清除编辑器初始内容，注意：如果focus属性设置为true,这个也为真，那么编辑器一上来就会触发导致初始化的内容看不到了
+            initialFrameWidth: null,
+            initialFrameHeight: 450,
+            BaseUrl: '',
+            UEDITOR_HOME_URL: 'static/ueditor/'
+          },
+          addFormVisible: false
+        }
+      },
+      methods: {
+        //获取文档内容
+        getContent: function(){
+          let content = this.$refs.ueditor.getUEContent();
+          console.log(content);
+          alert(content);
+        }
+      }
+  }
+
+</script>
