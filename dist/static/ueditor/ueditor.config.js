48e1217e56de45778efe0517c684fd01560f2414
liguoqing <liguoqing1@bluemoon.com.cn>
2018-12-24 16:47:19 +0800

    重新打包一个dist供preview

--- dist/static/ueditor/ueditor.config.js
+++ dist/static/ueditor/ueditor.config.js
@@ -31,7 +31,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "http://localhost:8081/ueditor/exec"
+        , serverUrl: "https://operationgwdev.bgycc.com/zuul/phantom-service-storage/ueditor/exec"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
