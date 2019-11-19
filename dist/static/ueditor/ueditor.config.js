9b229813088359be361ea2ad4f69bc087b9a492d
Guoqing.Lee <514471352@qq.com>
2019-11-19 18:22:28 +0800

    update url

--- dist/static/ueditor/ueditor.config.js
+++ dist/static/ueditor/ueditor.config.js
@@ -31,7 +31,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "https://operationgwdev.bgycc.com/zuul/phantom-service-storage/ueditor/exec"
+        , serverUrl: "http://161.117.224.252:8081/ueditor/exec"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
